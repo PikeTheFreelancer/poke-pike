b9dca2730d7ce6ef5776ee92e5803601d7690092
Pike <nghiavt1@arrowhitech.co>
2023-07-31 17:43:06 +0700

    initial

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout')
+@section('title', 'homepage')
+@section('content')
+  <h1>Pike</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\admin\DashboardController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomepageController::class, 'index'])->name('homepage');
+
+//Admin
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
+});
